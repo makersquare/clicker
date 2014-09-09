e0e9169ffcbd4f5d9ed857e932f238daf5a2c5e9
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-09 10:43:25 -0500

    added comment about alternative ENV variable usage

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	GITHUB_CONFIG = YAML.load_file("#{::Rails.root}/config/_github.yml")[::Rails.env]
 
+  # If you choose to use ENV variables, you can use the following format:
   # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,user:follow"
   provider :github, GITHUB_CONFIG['app_id'], GITHUB_CONFIG['secret'], scope: "user:email,user:follow"
 end

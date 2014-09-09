d4ca8d7936fc8ce1951ee019eaf3cf230a40aa48
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-08 20:03:41 -0500

    created user model, users and sessions controller; added helper method for current_user in appcontroller; added users' new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :class_groups, through: :memberships
   has_many :questions, through: :responses
+	
+	def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
+
 end

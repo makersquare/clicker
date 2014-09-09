ade9d549c29213558ccd083c954684beb12379b6
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-09 15:44:30 -0500

    added spec files for sessions controller and user&classgroup models, added spec and rails helper

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  describe '#create_with_omniauth' do
+    it "has a name, uid, provider" do
+      user = User.new
+
+      
+    end
+  end
+end

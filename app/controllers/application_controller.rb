487c415f63712c571bde7ec2d3324a77ec59e335
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-10 15:19:36 -0500

    added require_login method, added before_action filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :require_login
 
   private
+  
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to root_url # halts request cycle
+    end
+  end
+
+  def logged_in?
+    @current_user
+  end
  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]

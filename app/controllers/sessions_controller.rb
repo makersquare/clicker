0e883af3b1ca365408ad82119a4e36ca790ee4a9
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-10 15:19:57 -0500

    added optional skip_before_action for sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
+  # skip_before_action :require_login, only: [:new, :create]
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id     
+    session[:user_id] = user.id
     redirect_to root_url, :alert => "Signed in!"
   end
  

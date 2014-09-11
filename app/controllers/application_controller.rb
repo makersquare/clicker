3988f5f9a8eebe99d4191668935c1e26f78f27d0
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-11 11:36:38 -0500

    changed flash name from error to error_github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   
   def require_login
     unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
+      flash[:error_github] = "You must be logged in to access this section"
       redirect_to root_url # halts request cycle
     end
   end

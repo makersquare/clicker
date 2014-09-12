b63e767836c7de948c70aa1606cea4afe640fe10
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-12 09:43:29 -0500

    added before block, working on GET show_cohort8 testing

--- spec/controllers/class_group_controller_spec.rb
+++ spec/controllers/class_group_controller_spec.rb
@@ -2,10 +2,43 @@ require 'rails_helper'
 
 RSpec.describe ClassGroupsController, :type => :controller do
   
-  describe "GET show_cohort_8" do
+  before(:all) do
+    catelyn = User.create([
+      {
+        provider: "github",
+        uid: "9999999",
+        name: "Catelyn Tully",
+        nickname: "Cat",
+        verified: false
+      }])
+    ed = User.create([
+      {
+        provider: "github",
+        uid: "1111111",
+        name: "Ed Stark",
+        nickname: "Ned",
+        verified: true
+      }])
+    cohort8 = ClassGroup.create([
+      {
+        name: "Cohort 8, The Ocho"
+      }])
+  end
+
+
+  describe "GET show_cohort8" do
     it "returns http success" do
-      get :show_cohort_8
+      get :show, :id => 1
       expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
   end
 
@@ -15,6 +48,7 @@ RSpec.describe ClassGroupsController, :type => :controller do
       # Sign in the user
       post :create, :name => "My Class"
       expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
     
     it "does not allow unverified accounts" do

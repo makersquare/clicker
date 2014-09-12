c9e6e6978fc76b7251310228aa21dce70cb8d163
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-12 11:34:04 -0500

    Now working show/index and create methods for classgroup controller spec testing

--- spec/controllers/class_group_controller_spec.rb
+++ spec/controllers/class_group_controller_spec.rb
@@ -3,32 +3,29 @@ require 'rails_helper'
 RSpec.describe ClassGroupsController, :type => :controller do
   
   before(:all) do
-    catelyn = User.create([
-      {
-        provider: "github",
-        uid: "9999999",
-        name: "Catelyn Tully",
-        nickname: "Cat",
-        verified: false
-      }])
-    ed = User.create([
-      {
-        provider: "github",
-        uid: "1111111",
-        name: "Ed Stark",
-        nickname: "Ned",
-        verified: true
-      }])
-    cohort8 = ClassGroup.create([
-      {
-        name: "Cohort 8, The Ocho"
-      }])
+    @student = User.create(
+      provider: "github",
+      uid: "9999999",
+      name: "Catelyn Tully",
+      nickname: "Cat",
+      verified: false
+    )
+    @teacher = User.create(
+      provider: "github",
+      uid: "1111111",
+      name: "Ed Stark",
+      nickname: "Ned",
+      verified: true
+    )
+    @classgroup = ClassGroup.create(
+      name: "Cohort 8, The Ocho",
+      description: "MKS immersive course"
+    )
   end
 
-
   describe "GET show_cohort8" do
-    it "returns http success" do
-      get :show, :id => 1
+    it "responds successfully with an HTTP 200 status code" do
+      get :show, nil, {class_group_id: @classgroup.id}
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
@@ -36,7 +33,7 @@ RSpec.describe ClassGroupsController, :type => :controller do
 
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
-      get :index
+      get :index, nil, {user_id: @student.id}
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
@@ -46,9 +43,9 @@ RSpec.describe ClassGroupsController, :type => :controller do
     it "allows verified accounts" do
       User.create(:verified => true)
       # Sign in the user
-      post :create, :name => "My Class"
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
+      count = ClassGroup.all.count
+      post :create, {classgroup_id: @classgroup_id}
+      expect(count).to eq(1)
     end
     
     it "does not allow unverified accounts" do

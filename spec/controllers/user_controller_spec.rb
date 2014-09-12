30990b09f887afadabeb670e895463a39e0afac5
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-12 15:23:03 -0500

    removed any tests from Users controller spec; not ready to test for show/index/etc. methods (no view for this so far...)

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -18,14 +18,5 @@ RSpec.describe UsersController, :type => :controller do
       verified: true
     )
   end
-  describe "GET #show" do
-    it "responds successfully with an HTTP 200 status code" do
-        get :show, nil, {user_id: @student.id}
-        expect(response).to be_success
-        expect(response).to have_http_status(200)
-      end
-  end 
-    
-  
 
 end

756161e0ec262d771271228dca6c6433763fac39
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-12 17:04:35 -0500

    QuestionSets controller spec now working for questions_sets#index

--- spec/controllers/question_set_controller_spec.rb
+++ spec/controllers/question_set_controller_spec.rb
@@ -32,69 +32,21 @@ RSpec.describe QuestionSetsController, :type => :controller do
         answer: "true"
       }
     )
-    @mult_choice_question = MultiChoiceQuestion.create(
-    {
-      question_set_id: 1,
-      content: {
-        question: "Who invented Lambda Calculus?",
-        answer: "A",
-        choices: [
-          { "A" => "Alonzo Church" },
-          { "B" => "Linus Torvalds" },
-          { "C" => "Gabe Newell" },
-          { "D" => "Histocles" }
-        ]
-      }
-    },
-    {
-      question_set_id: 1,
-      content: {
-        question: "Lisp is not built on the concepts of lambda calculus.",
-        answer: "false"
-      }
-    }
-    )
-    @short_answer_question = ShortAnswerQuestion.create(
-      question_set_id: 1,
-      content: {
-            question: "What rule separates a binary search tree from a regular binary tree?",
-            answer: "All children to the left of the node must be smaller than the node, and all children to the right must be larger."
-      }
-    )
-    @response = Response.create([
-  {
-    question_id: 1,
-    user_id: 1,
-    content: {
-      response: "true"
-    }
-  },
-  {
-    question_id: 2,
-    user_id: 1,
-    content: {
-      response: "D"
-    }
-  },
-  {
-    question_id: 3,
-    user_id: 1,
-    content: {
-      response: "I have no idea."
-    }
-  }
-  ])
   end
-
 
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
-      get :index, nil, {user_id: @student.id}
+      get :index, {class_group_id: @class_group.id}, 
+        {user_id: @student.id}
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
   end
 
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+    end
+  end
 
 
 

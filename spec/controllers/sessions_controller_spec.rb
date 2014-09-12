def516c9383a526d5c4762b9f2aa3eb01606958c
Catherine O'Neill <cathzozo@gmail.com>
2014-09-12 16:55:56 -0500

    refactored repeated lines into before block

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,12 +11,14 @@ RSpec.describe SessionsController, :type => :controller do
                 :nickname => "Rand" 
       }
     })
+    session[:user_id] = nil
+    # WRAP IN VCR
+    post :create, provider: :github
   end
  
   describe "#create" do
  
     it "should successfully create a user" do
-      post :create, provider: :github
       expect(User.count).to eq(1)
       user = User.first
       expect(user.gravatar_id).to_not be_nil
@@ -24,22 +26,16 @@ RSpec.describe SessionsController, :type => :controller do
     end
  
     it "should successfully create a session" do
-      expect(session[:user_id]).to be_nil 
-      post :create, provider: :github
       expect(session[:user_id]).to_not be_nil
     end
  
     it "should redirect the user to the root url" do
-      post :create, provider: :github
       expect(response).to redirect_to(root_url)
     end
  
   end
  
   describe "#destroy" do
-    before do
-      post :create, provider: :github
-    end
  
     it "should clear the session" do
       expect(session[:user_id]).to_not be_nil

411b1891fee921a98d337439af597dd38c094607
Catherine O'Neill <cathzozo@gmail.com>
2014-09-12 16:03:11 -0500

    added gravatar id to get_user_spec, now 1 failing test

--- spec/txs/get_user_spec.rb
+++ spec/txs/get_user_spec.rb
@@ -8,17 +8,19 @@ describe GetUserInfo do
         user = GetUserInfo.run(params)
         expect(user.name).to eq("Gilbert")
         expect(user.uid).to eq("17013")
+        expect(user.gravatar_id).to eq("44d05cf62687c3489b863e4677414ca6")
       end
     end
   end
 
   context 'if user is already in DB' do
     it 'returns the corresponding user object' do
-      User.create(nickname: 'mindeavor', name: "Gilbert G.", uid: 1, verified: true, )
+      User.create(nickname: 'mindeavor', name: "Gilbert G.", uid: 1, verified: true, gravatar_id: "asdfjkl;")
       params = {'nickname' => 'mindeavor'}
       user = GetUserInfo.run(params)
       expect(user.name).to eq("Gilbert G.")
       expect(user.uid).to eq("1")
+      expect(user.gravatar_id).to eq("asdfjkl;")
     end
   end
 end

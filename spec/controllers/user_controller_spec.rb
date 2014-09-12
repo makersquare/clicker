a1e8ede898041d5254ce97d7cc8c22535be3e2eb
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-12 15:11:24 -0500

    Users controller: added before block, corrected wrongly named controller from ClassGroupsContoller to UsersController

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe ClassGroupsController, :type => :controller do
+RSpec.describe UsersController, :type => :controller do
   
-  describe "GET users/1" do
-    it "returns http success" do
-      get "users/1"
-      expect(response).to be_success
-    end
+  before do
+    @student = User.create(
+      provider: "github",
+      uid: "9999999",
+      name: "Catelyn Tully",
+      nickname: "Cat",
+      verified: false
+    )
+    @teacher = User.create(
+      provider: "github",
+      uid: "1111111",
+      name: "Ed Stark",
+      nickname: "Ned",
+      verified: true
+    )
+  end
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+        get :show, nil, {user_id: @student.id}
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
   end 
     
   

cb48e3a496c1eb04ecd9bd953044339b9eb3863c
Catherine O'Neill <cathzozo@gmail.com>
2014-09-13 19:23:09 -0500

    created before block variables

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.describe ResponsesController, :type => :controller do
 
   before do
-    @student = User.create(
+    @studenta = User.create(
       provider: "github",
       uid: "9999999",
       name: "Catelyn Tully",
       nickname: "Cat",
+      verified: false,
+      gravatar_id: "a4af3797c372cf36817f6767cefccc98"
+    )
+    @studentb = User.create(
+      provider: "github",
+      uid: "9999998",
+      name: "Molly Miller",
+      nickname: "mole",
       verified: false,
       gravatar_id: "a4af3797c372cf36817f6767cefccc98"
     )
@@ -23,7 +31,73 @@ RSpec.describe ResponsesController, :type => :controller do
       name: "Cohort 8, The Ocho",
       description: "MKS immersive course"
     )
+
+    Membership.create(
+      user_id: @studenta.id,
+      class_group_id: @classgroup.id,
+      kind: 'student'
+    )
+    Membership.create(
+      user_id: @studentb.id,
+      class_group_id: @classgroup.id,
+      kind: 'student'
+    )
+    Membership.create(
+      user_id: @teacher.id,
+      class_group_id: @classgroup.id,
+      kind: 'teacher'
+    )
+    #####1ST QUESTION SET WITH TWO QUESTIONS WITH TWO RESPONSES EACH#####
+    @questionset = QuestionSet.create(
+      class_group_id: @classgroup.id,
+      name: "RSpec testing"
+    )
+    @question1 = MultiChoiceQuestion.create(
+      question_set_id: @questionset.id
+    )
+    @response1a = Response.create(
+      question_id: @question1.id,
+      user_id: @studenta.id,
+      content: {"response"=>"D"}   
+    )
+    @response1b = Response.create(
+      question_id: @question1.id,
+      user_id: @studentb.id,
+      content: {"response"=>"C"}   
+    )
+    @question2 = ShortAnswerQuestion.create(
+      question_set_id: @questionset.id
+    )
+    @response2a = Response.create(
+      question_id: @question2.id,
+      user_id: @studenta.id,
+      content: {"response"=>"Here we go!"}   
+    )
+    @response2b = Response.create(
+      question_id: @question2.id,
+      user_id: @studentb.id,
+      content: {"response"=>"Woah! No way!"}   
+    )
+
+    #####2ND QUESTION SET WITH ONE QUESTION WITH TWO RESPONSES#####
+    @anotherquestionset = QuestionSet.create(
+      class_group_id: @classgroup.id,
+      name: "Jasmine testing"
+    )
+    @anotherquestion = AttendanceQuestion.create(
+      question_set_id: @anotherquestionset.id
+    )
+    @anotherresponsea = Response.create(
+      question_id: @anotherquestion.id,
+      user_id: @studenta.id,
+      content: {"response"=>"true"}   
+    )
+    @anotherresponseb = Response.create(
+      question_id: @anotherquestion.id,
+      user_id: @studentb.id,
+      content: {"response"=>nil}   
+    )    
   end
 
-  
+
 end

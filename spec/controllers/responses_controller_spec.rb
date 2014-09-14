c05cb39e00b9ac6390d416223de8d27d2d2e63c0
Catherine O'Neill <cathzozo@gmail.com>
2014-09-13 21:43:31 -0500

    #show renders template passes, put get path in before block

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -102,8 +102,11 @@ RSpec.describe ResponsesController, :type => :controller do
   end
 
   describe 'GET #show' do
-    it "responds successfully with an HTTP 200 status code" do
+    before do
       get :show, {question_set_id: @questionset, question_id: @question1, id: @response1a}, {user_id: @studenta}
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end 

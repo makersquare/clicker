aaa6b635851c80ddac5ac8812ec93080c2630dc5
Catherine O'Neill <cathzozo@gmail.com>
2014-09-13 20:49:43 -0500

    changed route for index test

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -111,7 +111,7 @@ RSpec.describe ResponsesController, :type => :controller do
 
   describe 'GET #index' do
     it "responds successfully with an HTTP 200 status code" do
-      get :show, {id: @classgroup.id}, {user_id: @student.id}
+      get :index, {question_set_id: @questionset.id, question_id: @question1.id}
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end    

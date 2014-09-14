049a50b65b99c45bca097efc3346746af89e8c7c
Catherine O'Neill <cathzozo@gmail.com>
2014-09-13 19:30:53 -0500

    added describe blocks for 5 methods

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -99,5 +99,18 @@ RSpec.describe ResponsesController, :type => :controller do
     )    
   end
 
+  describe 'GET #show' do
+  end
 
+  describe 'GET #index' do
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'PUT #update' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
 end

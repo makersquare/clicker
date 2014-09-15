a35ead54efbf6cfeb9ec533da624713c83ef4048
Catherine O'Neill <cathzozo@gmail.com>
2014-09-15 16:54:34 -0500

    set_question_set method in questions controller, and fixed path in index.json.builder

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :set_question_set
   before_action :require_login
+
   
   # GET /questions
   # GET /questions.json
@@ -57,6 +59,10 @@ class QuestionsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_question
       @question = Question.find(params[:id])
+    end
+
+    def set_question_set
+      @question_set = QuestionSet.find(params[:question_set_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/views/questions/index.json.jbuilder
+++ app/views/questions/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@questions) do |question|
-  json.extract! question, :id :type, :content
-  json.url question_url(question, format: :json)
+  json.extract! question, :id, :type, :content
+  json.url question_set_question_url(@question_set, question, format: :json)
 end

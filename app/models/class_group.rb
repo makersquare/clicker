0d3f4cdf7b9b2f12b709f3a58d6b8cdcc9c9d039
Christine Oen <oen.christine@gmail.com>
2014-09-15 16:05:20 -0500

    added class group model validation tests

--- app/models/class_group.rb
+++ app/models/class_group.rb
@@ -2,4 +2,5 @@ class ClassGroup < ActiveRecord::Base
   has_many :question_sets
   has_many :memberships
   has_many :users, through: :memberships
+  validates :name, :description, presence: true
 end
--- spec/models/classgroup_spec.rb
+++ spec/models/classgroup_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ClassGroup, :type => :model do
-  describe '#create_'
+  
+  describe 'Class Group' do
+    it 'creates a valid class group' do
+      valid_class = ClassGroup.create(
+        name: "Cohort 8",
+        description: "The Ocho"
+        )
+      expect(valid_class).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      invalid_class = ClassGroup.create(
+        name: "",
+        description: "The Ocho"
+        )
+      expect(invalid_class).to be_invalid
+    end
+
+    it 'is invalid without a description' do
+      invalid_class = ClassGroup.create(
+        name: "Cohort 8",
+        description: ""
+        )
+      expect(invalid_class).to be_invalid
+    end
+  end
 end

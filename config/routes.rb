1610f1bb663fc98e70383a8c32df52615f4fe434
Christine Oen <oen.christine@gmail.com>
2014-09-15 14:35:30 -0500

    added redirect to class groups index when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
+  
+  root :to => 'welcome#index'
 
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout

dd9f22e199af4db54ca8122bfc6479c37603917f
Catherine O'Neill <cathzozo@gmail.com>
2014-09-16 16:27:34 -0500

    changed to Faker::Number

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:user) do
   provider "github"
-  uid 123
+  uid {Faker::Number.number(5)}
   name { Faker::Name.name }
   verified false
   gravatar_id "7194e8d48fa1d2b689f99443b767316c"

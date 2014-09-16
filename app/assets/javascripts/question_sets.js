a57e8e52bedf5ce2b7e07352fc3e5f9b447bc0c4
Brian Patterson <brianpatterson2013@gmail.com>
2014-09-16 15:41:45 -0500

    Changed all instances of AppCtrl to QuestionSetCtrl in order to allow for other Controllers

--- app/assets/javascripts/question_sets.js
+++ app/assets/javascripts/question_sets.js
@@ -17,7 +17,7 @@ app.config(['$httpProvider', function(provider){
   provider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
 }]);
 
-app.controller('AppCtrl', function($scope, $location, QuestionSet, QuestionSetDelete){
+app.controller('QuestionSetCtrl', function($scope, $location, QuestionSet, QuestionSetDelete){
  $scope.questionSets = QuestionSet.query({ class_group_id: g.classGroup.id });
  $scope.addQuestionSet = function(){
   var questionSets = QuestionSet.save($scope.questionSet,{ class_group_id: g.classGroup.id, name: $scope.name }, function(response){

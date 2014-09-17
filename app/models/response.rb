08a5e138084ac774713d09f504e7125e8ae05644
Gilbert <gilbertbgarza@gmail.com>
2014-09-17 10:55:44 -0500

    Add notify method to Response model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,9 @@
 class Response < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  def notify
+    payload = Response.connection.quote(self.to_json)
+    Response.connection.raw_connection.exec("NOTIFY new_responses, #{payload}")
+  end
 end

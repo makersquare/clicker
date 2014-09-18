02c41e36ddc74ac4c79c8c302eebd4189ba7894b
Randy Davis <randy_davis@outlook.com>
2014-09-18 17:32:39 -0500

    Update memberships page to use ajax for removal of users

--- app/assets/javascripts/memberships.js
+++ app/assets/javascripts/memberships.js
@@ -1,14 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-
-// hide from view until click/touch actions
-$('.add-student-form').hide();
-
-console.log("I'm loading.");
-
-// -------------------------------------------------- CLICK ACTIONS
-
-
-
-// -------------------------------------------------- TOUCH ACTIONS
+$(document).on('ajax:success', function (e, data) {
+  console.log('Ajax Response Data:', data);
+});

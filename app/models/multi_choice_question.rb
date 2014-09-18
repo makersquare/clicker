92f1512c0cfe52683a0ea1d0f43cbdcbe1c38c36
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-18 00:39:53 -0500

    Reversed comparator to add error for answer outside index of choices array

--- app/models/multi_choice_question.rb
+++ app/models/multi_choice_question.rb
@@ -15,7 +15,7 @@ private
       errors.add(:absent_question, "Question must not be blank")
     elsif self.content["answer"].blank?
       errors.add(:absent_answer, "Answer must not be blank")
-    elsif self.content["answer"] <= self.content["choices"].count
+    elsif self.content["answer"] >= self.content["choices"].count
       errors.add(:wrong_answer, "Answer must be one of the answer choices")
     end
 

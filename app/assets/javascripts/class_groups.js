f140e3d9f6c3157e711d113d6354f1d83235e933
Allison Zadrozny <allison@zadrozny.com>
2014-09-18 09:54:13 -0500

    add styling and jQuery to pages with forms

--- app/assets/javascripts/class_groups.js
+++ app/assets/javascripts/class_groups.js
@@ -6,3 +6,36 @@
 
 // app.factory "QuestionSet", ($resource) ->
 //   $resource("question_set")
+
+$('#new-class-div').hide();
+
+// CLICK event handlers ------------------------------------------------ CLICK
+$('#add-a-class').click(function (e) {
+  // slides down add-class form
+  $('#new-class-div').animate({
+    height:'toggle'
+  });
+});
+
+// on click cancel button, hides the add-class form
+$('.new-class-cancel').click(function (e) {
+    $('#new-class-div').animate({
+    height:'toggle'
+    });
+});
+
+// TOUCH event handlers -------------------------------------------------- TOUCH
+$('#add-a-class').on('tap', function (e) {
+  // slides down add-class form
+  $('#new-class-div').animate({
+    height:'toggle'
+  });
+});
+
+// on TOUCH cancel button, hides the add-class form
+$('.new-class-cancel').on('tap', function (e) {
+    $('#new-class-div').animate({
+    height:'toggle'
+    });
+});
+  
--- app/assets/javascripts/memberships.js
+++ app/assets/javascripts/memberships.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+// hide from view until click/touch actions
+$('.add-student-form').hide();
+
+console.log("I'm loading.");
+
+// -------------------------------------------------- CLICK ACTIONS
+
+
+
+// -------------------------------------------------- TOUCH ACTIONS

2d106fb6ccea321a06e9675b0b1d23883071c6a6
Catherine O'Neill <cathzozo@gmail.com>
2014-09-18 17:08:38 -0500

    copied custom validator, partially done

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,9 +1,27 @@
 class Response < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+  validate :content_format
 
   def notify
     payload = Response.connection.quote(self.to_json)
     Response.connection.raw_connection.exec("NOTIFY new_responses, #{payload}")
+  end
+
+  private
+
+  def content_format
+    response = self.content["response"]
+
+    if self.question_set_id.blank? || self.question_set_id.class != Fixnum
+      errors.add(:absent_question_set_id, "Question Set ID is absent or wrong data type")
+    elsif response.blank?
+      errors.add(:absent_response, "Question must not be blank")
+    elsif answer.blank?
+      errors.add(:absent_answer, "Answer must not be blank")
+    elsif answer.to_i > self.content["choices"].count
+      errors.add(:wrong_answer, "Answer must be one of the answer choices")
+    end
+
   end
 end

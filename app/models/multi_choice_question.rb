a90151edf3801cfed460cb22daf55313c0e73495
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-17 21:07:07 -0500

    Added revised tests for MultiChoiceQuestion including new custom validation in MultiChoiceQuestion model

--- app/models/multi_choice_question.rb
+++ app/models/multi_choice_question.rb
@@ -1,2 +1,24 @@
 class MultiChoiceQuestion < Question
+  validate :content_format
+
+
+  # def find_choice(...)
+    
+  # end
+
+private
+
+  def content_format
+    if self.question_set_id.blank? || self.question_set_id.class != Fixnum
+      errors.add(:absent_question_set_id, "Question Set ID is absent or wrong data type")
+    elsif self.content["question"].blank?
+      errors.add(:absent_question, "Question must not be blank")
+    elsif self.content["answer"].blank?
+      errors.add(:absent_answer, "Answer must not be blank")
+      if self.content["answer"] <= self.content["choices"].count
+        errors.add(:wrong_answer, "Answer must be one of the answer choices")
+      end
+    end
+
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,33 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
 
-  before do
-    @question_set = Fabricate(:question_set, questions: 
-      [
-        Fabricate(:attendance_question), 
-        Fabricate(:short_answer_question), 
-        Fabricate(:multi_choice_question)
-      ]
-    )
-
-    @attendance_question = @question_set.questions[0]
-    @short_answer_question = @question_set.questions[1]
-    @multi_choice_question = @question_set.questions[2]
-  end
-
   describe "AttendanceQuestion's content validation" do
-    it "validates the question key/value" do
-      attendance_check = @attendance_question["content"]["question"] 
 
-      expect(@attendance_question.question_set_id).to eq(@question_set.id)
-      expect(attendance_check).to_not eq(nil)
+    let (:model) do
+      AttendanceQuestion.new(
+        :question_set_id => 1,
+        :content => {
+          :question => "Attendance Check",
+          :answer => 0,
+          :content => [
+            "true"
+          ]
+        }
+      )
+    end
+
+    it "validates the question key/value" do
+      expect(model.question_set_id).to_not eq(nil)
+      expect(model.content["question"]).to_not eq(nil)
     end
     it "validates absence of choice" do
-      expect(@attendance_question["content"]["choices"]).to eq(nil)
+      expect(model.content["choices"]).to eq(nil)
     end
 
     it "ensures the answer set to 'true'" do
-      expect(@attendance_question["content"]["answer"]).to eq("true")
+      # expect(@attendance_question["content"]["answer"]).to eq(0)
+      expect(model.content["answer"]).to eq(0)
     end
   end
 
@@ -48,32 +47,65 @@ RSpec.describe Question, :type => :model do
   end
 
   describe "MultiChoiceQuestion's content validation" do
-    it "validates the question key/value" do
-      multi_choice_check = @multi_choice_question["content"]
-
-      expect(@multi_choice_question.question_set_id).to eq(@question_set.id)
-      expect(multi_choice_check).to_not eq(nil)
+    
+    let(:model) do
+      MultiChoiceQuestion.new(
+        :question_set_id => 1,
+        :content => {
+          :question => "What is the meaning of life?",
+          :answer  => 0,
+          :choices => [
+            "Every rose has its thorn",
+            "Life is meaningless",
+            "Life just is",
+            "Loving, learning, earning, and realizing"
+          ]
+        }
+      )
     end
-    it "validates the choices key/value" do
-      multi_choices = @multi_choice_question["content"]["choices"]
-
-      expect(multi_choices).to_not eq(nil)
-
-      multi_choices_keys = multi_choices.map { |x| x.keys }
-      multi_answers = multi_choices.map { |x| x.values }
-
-      expect(multi_choices_keys.count).to eq(multi_answers.count)
+    
+    it "validates with a perfect format" do
+    # binding.pry
+      expect(model).to be_valid
+      # expect(model).to_not be_valid
+      # expect(model.errors).to have_key(:nope)
     end
 
-    it "ensures the answer index is within the number of choices" do
-      multi_choices_check = @multi_choice_question["content"]
+    describe "Validation" do
+      it "validates the question key/value" do
+        multi_choice_check = @multi_choice_question["content"]
 
-      choices = multi_choices_check["choices"]
-      answer = multi_choices_check["answer"]
+        expect(@multi_choice_question.question_set_id).to eq(@question_set.id)
+        expect(multi_choice_check).to_not eq(nil)
+      end
 
-      confirm = choices.map{ |x| x.keys }.flat_map{ |e| e }.find{ |i| i == answer }
+      it "validates the choices key/value" do
+        model.choices = []
+        multi_choices = @multi_choice_question["content"]["choices"]
 
-      expect(confirm).to_not eq(nil) 
+        expect(multi_choices).to_not eq(nil)
+
+        multi_choices_keys = multi_choices.map { |x| x.keys }
+        multi_answers = multi_choices.map { |x| x.values }
+
+        expect(multi_choices_keys.count).to eq(multi_answers.count)
+      end
+
+      it "ensures the answer index is within the number of choices" do
+      end
+    end
+
+    describe "#find_choice" do
+      it "finds a choice by .." do
+        multi_choices_check = @multi_choice_question["content"]
+
+        choices = multi_choices_check["choices"]
+        answer = multi_choices_check["answer"]
+
+        confirm = choices.map{ |x| x.keys }.flat_map{ |e| e }.find{ |i| i == answer }
+
+        expect(confirm).to_not eq(nil)
+      end
     end
   end
 

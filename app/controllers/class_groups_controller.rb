94e6befc701aa1a38795a9f4c8ce43720509f492
Catherine O'Neill <cathzozo@gmail.com>
2014-09-18 18:21:29 -0500

    we have a session table with a user id

--- app/controllers/class_groups_controller.rb
+++ app/controllers/class_groups_controller.rb
@@ -6,6 +6,7 @@ class ClassGroupsController < ApplicationController
   # GET /class_groups
   # GET /class_groups.json
   def index
+      # raise 'c'
     @class_groups = @current_user.class_groups
     @class_group = ClassGroup.new
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    #this is in the browser session
     session[:user_id] = user.id
+    #this is in the DB session
+    my_session = Session.new
+    my_session.user_id = user.id
+    my_session.save
     UpdateUserInfo.run(user)
     redirect_to root_url, :alert => "Signed in!"
   end

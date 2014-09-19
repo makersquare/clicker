78294cda63b2c11409adf05a603916d9d4f1ced3
Allison Zadrozny <allison@zadrozny.com>
2014-09-19 11:49:14 -0500

    delete n3-charts to restart process

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ["ngResource", "ngRoute"]);
+var app = angular.module('app', ["ngResource", "ngRoute", 'n3-pie-chart']);
 
 app.config(['$httpProvider', function(provider){
   provider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');

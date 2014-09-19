3c11d239839f2e5b251e7318e1c8320d8b7ec6e0
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-19 15:47:16 -0500

    modified spec title to be for student

--- spec/controllers/class_group_controller_spec.rb
+++ spec/controllers/class_group_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe ClassGroupsController, :type => :controller do
   end
 
   describe "GET #show" do
-    it "responds successfully with an HTTP 200 status code" do
+    it "responds successfully with an HTTP 200 status code for student" do
       get :show, {id: @class_group.id}, {user_id: @student.id}
       expect(response).to be_success
       expect(response).to have_http_status(200)

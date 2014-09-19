ecf7ad6383ac2089ea3ab72f77f18fcf6e6e6034
Catherine O'Neill <cathzozo@gmail.com>
2014-09-19 14:03:42 -0500

    index and edit views working

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users
+  resources :users, only: [:index, :show, :edit]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

046fe55a4a4479cc16a38953ffdedc23b5bdb51c
Catherine O'Neill <cathzozo@gmail.com>
2014-09-19 15:24:11 -0500

    removed depending on order from student flow spec, closes #177

--- spec/requests/student_flow_spec.rb
+++ spec/requests/student_flow_spec.rb
@@ -4,17 +4,25 @@ require 'rails_helper'
 RSpec.describe "student flow" do
   
   before do
-    @student = Fabricate(:user, class_groups: [
-      Fabricate(:class_group, question_sets: [
-        Fabricate(:question_set),
-        Fabricate(:question_set, questions: [
-          Fabricate(:attendance_question),
-          Fabricate(:multi_choice_question),
-          Fabricate(:multi_choice_question)
-        ])
-      ]), 
-      Fabricate(:class_group)
-    ])
+    @student = Fabricate(:user)
+    @class_group1 = Fabricate(:class_group)
+    @qset1 = Fabricate(:question_set, class_group_id: @class_group1.id)
+    @qset2 = Fabricate(:question_set, class_group_id: @class_group1.id, state: 'open')
+    
+    @q1 = Fabricate(:attendance_question, question_set_id: @qset2.id)
+    @q2 = Fabricate(:multi_choice_question, question_set_id: @qset2.id)
+    @q3 = Fabricate(:multi_choice_question, question_set_id: @qset2.id)
+  
+    @class_group2 = Fabricate(:class_group)
+
+    Membership.create(
+      class_group_id: @class_group1.id,
+      user_id: @student.id,
+      kind: 'student')
+    Membership.create(
+      class_group_id: @class_group2.id,
+      user_id: @student.id,
+      kind: 'student')
   end
 
   let(:session) do
@@ -43,38 +51,34 @@ RSpec.describe "student flow" do
       #logs in & redirects to class_groups_index
       get "/test_sign_in/#{@student.id}"
       get '/'
-      class1 = @student.class_groups.first
-      class2 = @student.class_groups[1]
       expect(response).to redirect_to('/class_groups')
       follow_redirect!
-      expect(response.body).to include(class1.name)
-      expect(response.body).to include(class2.name)
+      expect(response.body).to include(@class_group1.name)
+      expect(response.body).to include(@class_group2.name)
 
       #student views 1 class group on show page and the question sets for that class
-      get "/class_groups/#{class1.id}"
+      get "/class_groups/#{@class_group1.id}"
       expect(response).to render_template('class_groups/show')
 
       #if a question set is active the student can view the page for that question set
-      active_qset = class1.question_sets[1]
-      q1 = active_qset.questions[0]
-      q2 = active_qset.questions[1]
-      q3 = active_qset.questions[2]
-      get "/class_groups/#{class1.id}/question_sets/#{active_qset.id}"
+      get "/class_groups/#{@class_group1.id}/question_sets/#{@qset2.id}"
       expect(response).to render_template('question_sets/show')
       
       #student answers questions
-      post "question_sets/#{active_qset.id}/questions/#{q1.id}/responses.json", :response => {
-        question_id: q1.id, user_id: @student.id, content: {response: "D"}}
-      post "question_sets/#{active_qset.id}/questions/#{q2.id}/responses.json", :response => {
-        question_id: q2.id, user_id: @student.id, content: {response: "C"}}
-      post "question_sets/#{active_qset.id}/questions/#{q3.id}/responses.json", :response => {
-        question_id: q3.id, user_id: @student.id, content: {response: "A"}}
-      expect(q1.responses.count).to eq(1)
-      expect(q2.responses.count).to eq(1)
-      expect(q3.responses.count).to eq(1)
+      post "question_sets/#{@qset2.id}/questions/#{@q1.id}/responses.json", :response => {
+        question_id: @q1.id, user_id: @student.id, content: {response: "D"}}
+      post "question_sets/#{@qset2.id}/questions/#{@q2.id}/responses.json", :response => {
+        question_id: @q2.id, user_id: @student.id, content: {response: "C"}}
+      post "question_sets/#{@qset2.id}/questions/#{@q3.id}/responses.json", :response => {
+        question_id: @q3.id, user_id: @student.id, content: {response: "A"}}
+      expect(@q1.responses.count).to eq(1)
+      expect(@q2.responses.count).to eq(1)
+      expect(@q3.responses.count).to eq(1)
       these_responses = Response.where(user_id: @student.id)
-      expect(these_responses[0].content["response"]).to eq("D")
       expect(these_responses.count).to eq(3)
+
+      response1 = Response.find_by(question_id: @q1.id, user_id: @student.id)
+      expect(response1.content["response"]).to eq("D")
     end
   end
 

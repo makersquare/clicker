daa6ccc7fb6c7f84e394e1c226f659924c858f32
Gilbert <gilbertbgarza@gmail.com>
2014-09-19 11:08:22 -0500

    Update postgres matcher to accept blocks

--- spec/support/postgres_matchers.rb
+++ spec/support/postgres_matchers.rb
@@ -1,39 +1,68 @@
 require 'rspec/expectations'
 
 # NOTE: This matcher does not work for multiple NOTIFY statements on the same channel
-RSpec::Matchers.define :notify do |channel,payload|
+# RSpec::Matchers.define :notify do |channel,payload,&match_block|
+module NotifyMatcher
 
-  supports_block_expectations
-  def diffable?
-    @notified == true
+  def notify(*args, &block)
+    Notify.new(*args, &block)
   end
 
-  match do |code|
-    pg = PGconn.open(:dbname => 'clicker_test')
-    pg.exec "LISTEN #{channel}"
-    @notified = false
+  class Notify
 
-    code.call
+    def initialize(channel, payload=nil, &payload_block)
+      @channel = channel
+      @payload = payload
+      @payload_block = payload_block if block_given?
+    end
 
-    wait = Proc.new do
-      pg.wait_for_notify(0.5) do |actual_channel, pid, actual_payload|
-        return wait if channel != actual_channel
+    def supports_block_expectations?
+      true
+    end
 
-        @notified = true
-        @actual = actual_payload
-        expect(actual_payload).to eq payload
+    def diffable?
+      @notified == true
+    end
+
+    def matches?(code)
+      pg = PGconn.open(:dbname => 'clicker_test')
+      pg.exec "LISTEN #{@channel}"
+      @notified = false
+
+      code.call
+
+      payload_matches = nil
+
+      wait = Proc.new do
+        pg.wait_for_notify(0.5) do |actual_channel, pid, actual_payload|
+          return wait if @channel != actual_channel
+
+          @notified = true
+          @actual = actual_payload
+          
+          payload_matches = actual_payload == @payload if @payload
+          @payload_block.call(actual_payload) if @payload_block
+        end
+      end
+
+      wait.call
+      if @payload
+        @notified == true && payload_matches == true
+      else
+        @notified == true
       end
     end
 
-    wait.call
-    expect(@notified).to eq true
-  end
-
-  failure_message do
-    if @notified == false
-      "Expected a NOTIFY on channel `#{channel}` (received nothing on that channel instead)"
-    else
-      "Expected a NOTIFY on channel `#{channel}`\n\tExpected payload: #{payload.inspect}\n\tGot payload: #{@actual.inspect}"
+    def failure_message
+      if @notified == false
+        "Expected a NOTIFY on channel `#{@channel}` (received nothing on that channel instead)"
+      else
+        "Expected a NOTIFY on channel `#{@channel}`\n\tExpected payload: #{@payload.inspect}\n\tGot payload: #{@actual.inspect}"
+      end
     end
   end
+end
+
+RSpec.configure do |config|
+  config.include(NotifyMatcher)
 end

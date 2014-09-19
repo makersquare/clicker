cf39dccb814300e9bd6ed3a982fc5a2c0fa70801
Brian Patterson <brianpatterson2013@gmail.com>
2014-09-19 11:32:01 -0500

    Got server side events to work with a start and stop button

--- node/server.js
+++ node/server.js
@@ -1,20 +1,66 @@
 //load dependencies
 
-var express = require('express');
+// var express = require('express');
+// var http = require('http');
+// var os = require('os');
+// var path = require('path');
+
+// //create the app
+// var app = express();
+
+// //intro test
+
+// app.get('/', function(req,res){
+//   res.send('hello world');
+// });
+
+// app.listen(4000);
+
 var http = require('http');
-var os = require('os');
-var path = require('path');
+var fs = require('fs');
+ 
+/*
+ * send interval in millis
+ */
+var sendInterval = 1000;
+ 
+function sendServerSendEvent(req, res) {
+  res.writeHead(200, {
+  'Content-Type' : 'text/event-stream',
+  'Cache-Control' : 'no-cache',
+  'Connection' : 'keep-alive'
+  });
 
-//create the app
-var app = express();
+  var sseId = (new Date()).toLocaleTimeString();
 
-//intro test
+  setInterval(function() {
+  writeServerSendEvent(res, sseId);
+  }, sendInterval);
 
-app.get('/', function(req,res){
-  res.send('hello world');
-});
-
-app.listen(4000);
+  writeServerSendEvent(res, sseId);
+}
+ 
+function writeServerSendEvent(res, sseId) {
+  res.write('id: ' + sseId + '\n');
+  res.write("data: Brian is awesome." + '\n\n');
+}
+ 
+http.createServer(function(req, res) {
+  if (req.headers.accept && req.headers.accept == 'text/event-stream') {
+  if (req.url == '/talk') {
+  sendServerSendEvent(req, res);
+  } else {
+  res.writeHead(404);
+  res.end();
+  }
+  } else {
+  res.writeHead(200, {
+  'Content-Type' : 'text/html'
+  });
+  res.write(fs.readFileSync(__dirname + '/index.html'));
+  res.end();
+  }
+}).listen(8080);
 
 //introductory configuration
 
